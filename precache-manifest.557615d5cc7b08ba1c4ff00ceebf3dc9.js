1825dfb90cdd20c3b5a207ba645e84f6f5995a36
Wai Lun <suenwailun@gmail.com>
2019-04-06 22:32:04 +0800

    Updates

---
+++ precache-manifest.557615d5cc7b08ba1c4ff00ceebf3dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fe508da8a2263f5574a",
+    "url": "/ehr-diabetes-alert-demo/static/js/runtime~main.ad3aad5a.js"
+  },
+  {
+    "revision": "de04f6c29c82acb8f5be",
+    "url": "/ehr-diabetes-alert-demo/static/js/main.c1237f98.chunk.js"
+  },
+  {
+    "revision": "07ce47816ddd749cb33d",
+    "url": "/ehr-diabetes-alert-demo/static/js/2.61725264.chunk.js"
+  },
+  {
+    "revision": "de04f6c29c82acb8f5be",
+    "url": "/ehr-diabetes-alert-demo/static/css/main.4fefc15e.chunk.css"
+  },
+  {
+    "revision": "07ce47816ddd749cb33d",
+    "url": "/ehr-diabetes-alert-demo/static/css/2.b1e39f4a.chunk.css"
+  },
+  {
+    "revision": "683c90a74d802ef48533f7aceec3f1ee",
+    "url": "/ehr-diabetes-alert-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/suenwl/ehr-diabetes-alert-demo/precache-manifest.65184cbc5b704360ecef9607ef847a15.js"
+  "/ehr-diabetes-alert-demo/precache-manifest.557615d5cc7b08ba1c4ff00ceebf3dc9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/suenwl/ehr-diabetes-alert-demo/index.html", {
+workbox.routing.registerNavigationRoute("/ehr-diabetes-alert-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
