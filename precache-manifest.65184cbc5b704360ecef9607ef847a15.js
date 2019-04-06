a4d88167694b7c1b96fd35ba4891044a96c855d7
Wai Lun <suenwailun@gmail.com>
2019-04-06 22:28:04 +0800

    Updates

---
+++ precache-manifest.65184cbc5b704360ecef9607ef847a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d047f4df58d2a8e4d640",
+    "url": "/suenwl/ehr-diabetes-alert-demo/static/js/runtime~main.a5fd0bc9.js"
+  },
+  {
+    "revision": "d0a1405dba24320789a3",
+    "url": "/suenwl/ehr-diabetes-alert-demo/static/js/main.460e9a84.chunk.js"
+  },
+  {
+    "revision": "891feca8f3b7f2cb41b7",
+    "url": "/suenwl/ehr-diabetes-alert-demo/static/js/2.ec028795.chunk.js"
+  },
+  {
+    "revision": "d0a1405dba24320789a3",
+    "url": "/suenwl/ehr-diabetes-alert-demo/static/css/main.4fefc15e.chunk.css"
+  },
+  {
+    "revision": "891feca8f3b7f2cb41b7",
+    "url": "/suenwl/ehr-diabetes-alert-demo/static/css/2.b1e39f4a.chunk.css"
+  },
+  {
+    "revision": "be7fd76817117fbfecd1d2be55af5cb5",
+    "url": "/suenwl/ehr-diabetes-alert-demo/index.html"
+  }
+];
